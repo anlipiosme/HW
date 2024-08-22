fcca076d424ba0ddd9a660d67bea05e87fd08360
Anna <anlipios@yandex.ru>
2024-08-23 02:15:53 +0700

    л6

---
+++ src/Park.java
@@ -0,0 +1,11 @@
+public class Park {
+    String name;
+    String city;
+    public static class Attraction {
+        String attractionName;
+        String attractionTime;
+        int AttractionPrice;
+
+    }
+
+}
